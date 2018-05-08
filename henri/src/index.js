5c38204bf78d6e62e8fcf19091b07c7bce97782f
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-05-08 19:13:02 +0800

    x

--- henri/src/index.js
+++ henri/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App name="Barrow" />, document.getElementById('root'));
 registerServiceWorker();
