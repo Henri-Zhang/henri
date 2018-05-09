0ab44824f9fc652cb9e11460ea59e325dba01073
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-05-09 14:39:50 +0800

    bootstrap

--- henri/src/index.js
+++ henri/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
