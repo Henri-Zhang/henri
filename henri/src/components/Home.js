90eb1298f7eeb41cb83e0997b9895c3d9947afac
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-05-09 08:28:44 +0800

    Home component.

---
+++ henri/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './../styles/Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <h1>Honululu.</h1>
+    );
+  }
+}
+
+export default Home;
--- henri/src/index.js
+++ henri/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App name="Barrow" />, document.getElementById('root'));
+ReactDOM.render(<Home name="Barrow" />, document.getElementById('root'));
 registerServiceWorker();
