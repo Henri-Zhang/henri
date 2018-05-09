91534be5d22f9ee790d8e604c91430d6b7016bf9
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-05-09 11:08:56 +0800

    d

--- henri/src/components/Home.js
+++ henri/src/components/Home.js
@@ -4,7 +4,22 @@ import './../styles/Home.css';
 class Home extends Component {
   render() {
     return (
-      <h1>Honululu.</h1>
+      <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="/">
+              <i class="icon-logo1"></i>
+              <i class="icon-logo5"></i>
+            </a>
+          </div>
+          <div class="collapse navbar-collapse hide">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a class="btn btn-one" href="/login/">Sign In</a></li>
+              <li><a class="btn btn-two" href="/management-software/">JOIN TOPVET</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
