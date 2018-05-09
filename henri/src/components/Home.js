4cff0704685b8a793630aad16a36535bab843353
barrow.zhang <371595867@qq.com>
2018-05-09 22:42:21 +0800

    navbar

--- henri/src/components/Home.js
+++ henri/src/components/Home.js
@@ -2,26 +2,26 @@ import React, { Component } from 'react';
 import './../styles/Home.scss';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {scrolled : false}
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (event) => {
+    this.setState(prevState => ({
+      scrolled: !prevState.scrolled
+    }));
+  }
+
   render() {
     return (
-      <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="/">
-              <i className="icon-logo1"></i>
-              <i className="icon-logo5"></i>
-            </a>
-          </div>
-          <div className="collapse navbar-collapse">
-            <ul className="nav navbar-nav navbar-right">
-              <li><a className="btn btn-one" href="/login/">Sign In</a></li>
-              <li><a className="btn btn-two" href="/management-software/">JOIN TOPVET</a></li>
-            </ul>
-          </div>
-        </div>
+      <nav className={this.state.scrolled ? "navbar navbar-default navbar-fixed-top scrolled" : "navbar navbar-default navbar-fixed-top"} onClick={this.handleClick}>
+        <h1>Welcome to Lanchow.</h1>
       </nav>
     );
   }
+  
 }
 
 export default Home;
--- henri/src/index.js
+++ henri/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Home from './components/Home';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+ReactDOM.render(<App name="Barrow" />, document.getElementById('root'));
 ReactDOM.render(<Home name="Barrow" />, document.getElementById('root'));
 registerServiceWorker();
