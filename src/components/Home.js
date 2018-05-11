b10b23c67943998c9ef6c6b5fe30beecc58adb7d
barrow.zhang <371595867@qq.com>
2018-05-11 23:05:47 +0800

    xc

--- src/components/Home.js
+++ src/components/Home.js
@@ -38,29 +38,37 @@ class Home extends Component {
               <div className="row">
                 <div className="col-sm-12 item">
                   <h1>Nice to meet you.</h1>
-                  <h2>And my name is henri.</h2>
+                  <h2>And my name is Henri.</h2>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="carousel">
+        <div className="hobbies-interests">
           <div className="container">
             <div className="row">
               <div className="col-sm-12">
                 <div className="title">My hobbies and interests</div>
               </div>
             </div>
-            <div>
+            <div className="row carousel">
               <a className="slide-arrow prev">
-                <span>
-                  <FontAwesome name="arrow-left" />
-                </span>
+                <FontAwesome name="arrow-left" />
               </a>
+              <div className="item">
+                <img alt="jog" src={require("./../asserts/images/jog.png")} />
+                <div className="name">Jog</div>
+              </div>
+              <div className="item">
+                <img alt="delicacy" src={require("./../asserts/images/jog.png")} />
+                <div className="name">Delicacy</div>
+              </div>
+              <div className="item">
+                <img alt="travel" src={require("./../asserts/images/jog.png")} />
+                <div className="name">Travel</div>
+              </div>
               <a className="slide-arrow next">
-                <span>
-                  <FontAwesome name="arrow-right" />
-                </span>
+                <FontAwesome name="arrow-right" />
               </a>
             </div>
           </div>
