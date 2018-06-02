103226793e74d10f875dfc50643396b0540ed971
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-06-02 16:04:00 +0800

    aqwe

---
+++ src/components/ComingSoon.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ComingSoon extends Component {
+  render() {
+    return (
+      <h1>Coming soon...</h1>
+    );
+  }
+}
+
+export default ComingSoon;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home name="Henri" />, document.getElementById('root'));
+const Home = Loadable({
+  loader: () => import('./components/Home'),
+  loading: () => <div>Loading...</div>
+})
+
+const RunningStory = Loadable({
+  loader: () => import('./components/ComingSoon'),
+  loading: () => <div>Loading...</div>
+})
+
+const Places = Loadable({
+  loader: () => import('./components/ComingSoon'),
+  loading: () => <div>Loading...</div>
+})
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/running-story" component={RunningStory} />
+      <Route path="/places" component={Places} />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
