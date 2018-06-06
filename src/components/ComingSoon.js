0daaa59fbe399e4c0b2808c35d263059b068aa47
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-06-06 14:25:06 +0800

    get client broswer ip.

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class ComingSoon extends Component {
+  componentDidMount() {
+    axios.get('http://freegeoip.net/json/').then(response => {
+      console.log(response)
+    })
+  }
+
   render() {
     return (
       <h1>Coming soon...</h1>
