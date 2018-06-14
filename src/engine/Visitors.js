bd5f98053488192aceca9dd774e6790d167ac19b
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-06-14 16:14:20 +0800

    sda

--- src/engine/Visitors.js
+++ src/engine/Visitors.js
@@ -1,5 +1,12 @@
+import axios from 'axios'
+
 export default class Visitors {
   static addVisitor = info => {
     info.date = new Date().toLocaleString()
+    axios.post('http://vps.henri.ren:8080/add-visitor', info).then(response => {
+      console.log(response.data)
+    }).catch(error => {
+      console.log(error)
+    })
   }
 }
