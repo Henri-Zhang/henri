71f3a2912f78f7c95bd73545c6933cd8a3730faf
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-06-20 13:22:00 +0800

    dsa

--- src/engine/Visitors.js
+++ src/engine/Visitors.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default class Visitors {
   static addVisitor = info => {
     info.date = new Date().toLocaleString()
-    axios.post('http://vps.henri.ren:5000/visitors', info).then(response => {
+    axios.post('https://vps.henri.ren:5000/visitors', info).then(response => {
       // console.log(response.data)
     }).catch(error => {
       // console.log(error)
