fe83e705eebe4a6214ff5eef77147f874c40b48b
barrow.zhang <barrowzhang@augmentum.com.cn>
2018-07-06 14:16:03 +0800

    2018-07-06 14:16:03

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './../styles/Loading.scss'
 
 export default (props) => {
   if (props.error) {
     return (<div>Error! <button onClick={ props.retry }>Retry</button></div>)
-  } else if (props.pastDelay) {
-    return (<div>Loading...</div>)
+  } else if (!props.pastDelay) {
+    return (
+      <h2 id="loading">Loading...</h2>
+    )
   } else {
     return null;
   }
