a342607d3889c08ef00ac2ccddae6f21a98bf995
heng.zhang <heng.zhang@ximalaya.com>
2019-08-10 21:03:13 +0800

    chore: webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
+    chunkFilename: '[id].chunk.[hash].js',
     path: path.resolve('dist')
   },
   module: {
@@ -43,6 +44,13 @@ module.exports = {
       }
     ]
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      maxSize: 244 * 1024,
+      name: true
+    }
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
@@ -52,7 +60,16 @@ module.exports = {
   ],
   devServer: {
     historyApiFallback: true,
+    host: '0.0.0.0',
+    useLocalIp: true,
+    open: true,
+    quiet: true,
+    progress: true,
     compress: true,
-    port: 9000
+    port: 9000,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
   }
 };
